c87a805ecbbf24c563831ba4eb333b8926b9bf13
0-0REL <rodrigo12337@outlook.com>
2025-04-10 23:40:57 -0600

    pid

--- Vision/dron.py
+++ Vision/dron.py
@@ -1,14 +1,18 @@
-# Programa en media pipe que funciona segun como muestra la documentacion que de debe configurar
-# y usar
+# Programa que para detectar y coordinar dron mediante el rostro detectado
 import mediapipe as mp
 import cv2
 import time
 import numpy as np
+from simple_pid import PID
 
 START_TIME = time.time()
 DETECTION_RESULT = None
 
+# Configuraciones
 cam = cv2.VideoCapture(0)
+pid_thr = PID(1, 0, 0, 0, output_limits=(1e3,2e3)) # Set 0 distancia en x
+pid_yaw = PID(1, 0, 0, 0, output_limits=(-500,500)) # set 0 distancia en y
+pid_pitch = PID(0.5, 0, 0, 13e3, output_limits=(-500,500)) # set area a distancia permitida
 
 BaseOptions = mp.tasks.BaseOptions
 FaceDetector = mp.tasks.vision.FaceDetector
@@ -17,12 +21,12 @@ FaceDetectorResult = mp.tasks.vision.FaceDetectorResult
 VisionRunningMode = mp.tasks.vision.RunningMode
 
 # Create a face detector instance with the live stream mode:
-def print_result(result: FaceDetectorResult, output_image: mp.Image, timestamp_ms: int):
+def resultado(result: FaceDetectorResult, output_image: mp.Image, timestamp_ms: int):
     global DETECTION_RESULT
     DETECTION_RESULT = result
     #print('face detector result: {}'.format(result))
 
-def visualize(image, detection_result) -> np.ndarray:
+def mostrar(imagen, detection_result) -> np.ndarray:
     """Draws bounding boxes on the input image and return it.
     Args:
         image: The input RGB image.
@@ -31,22 +35,22 @@ def visualize(image, detection_result) -> np.ndarray:
         Image with bounding boxes.
     """
     if detection_result is None:
-        return image
+        return imagen
         
-    for detection in detection_result.detections:
-        # Draw bounding_box
-        bbox = detection.bounding_box
-        start_point = bbox.origin_x, bbox.origin_y
-        end_point = bbox.origin_x + bbox.width, bbox.origin_y + bbox.height
-        # Use the orange color for high visibility.
-        cv2.rectangle(image, start_point, end_point, (0, 165, 255), 3)
+    for deteccion in detection_result.detections:
+        # Dibuja un punto al centro de la cara
+        bbox = deteccion.bounding_box
+        punto = bbox.origin_x + bbox.width//2, bbox.origin_y + bbox.height//2
+        cv2.circle(imagen, punto, 5, (0, 0, 255), -1)
+        Area = bbox.width*bbox.height
+        print('Area:', Area)
+        print('adelante:', pid_pitch(Area))
     
-    return image
+    return imagen
 
 options = FaceDetectorOptions(
-    base_options=BaseOptions(model_asset_path='blaze_face_short_range.tflite'),
-    running_mode=VisionRunningMode.LIVE_STREAM,
-    result_callback=print_result)
+    base_options=BaseOptions(model_asset_path='blaze_face_short_range.tflite', delegate=mp.tasks.BaseOptions.Delegate.GPU),
+    running_mode=VisionRunningMode.LIVE_STREAM, result_callback=resultado)
 
 with FaceDetector.create_from_options(options) as detector:
     while True:
@@ -63,9 +67,9 @@ with FaceDetector.create_from_options(options) as detector:
         detector.detect_async(mp_image, frame_timestamp_ms)
         
         if DETECTION_RESULT:
-            frm = visualize(frm, DETECTION_RESULT)
+            frm = mostrar(frm, DETECTION_RESULT)
 
-        cv2.imshow('face_detection', cv2.flip(frm,1))
+        cv2.imshow('Rostro',frm)
 
         # Stop the program if the ESC key is pressed.
         if cv2.waitKey(1) == 27:
