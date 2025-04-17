4c8d02977f0cc37137cde00552a74dd58dcc9ebf
0-0REL <rodrigo12337@outlook.com>
2025-04-16 18:15:24 -0600

    prueba con pymavlink

--- comunicacion/conec.py
+++ comunicacion/conec.py
@@ -2,8 +2,11 @@
 from pymavlink import mavutil
 
 # Start a connection listening on a UDP port
-connection = mavutil.mavlink_connection('/dev/ttyACM0')
-
+try:
+    connection = mavutil.mavlink_connection('/dev/ttyUSB0', baud=57600)
+except Exception as e:
+    print("Error connecting to the vehicle: ", e)
+    exit(1)
 # Wait for the first heartbeat to set the system and component ID of remote system for the link
 connection.wait_heartbeat()
 print("Heartbeat from system (system %u component %u)" % (connection.target_system, connection.target_component))
