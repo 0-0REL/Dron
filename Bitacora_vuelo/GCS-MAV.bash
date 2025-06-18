0c5b490d590ab9b7cd62c5299cb7324b1c8dbd30
0-0REL <rodrigo12337@outlook.com>
2025-06-18 15:10:58 -0600

    dron

--- Bitacora_vuelo/GCS-MAV.bash
+++ Bitacora_vuelo/GCS-MAV.bash
@@ -1,5 +1,4 @@
 #!/bin/bash
-cd ~/Documentos/Dron/Bitacora_vuelo/
 
 #mavproxy.py --master=/dev/ttyUSB0,57600 --out 127.0.0.1:14550 --console --aircraft=Dron
 mavproxy.py --master=192.168.68.103:14650 --out 127.0.0.1:14550 --console --aircraft=Dron
