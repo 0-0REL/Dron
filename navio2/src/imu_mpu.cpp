dfa7f658afccc2e209bcb3aaa9f20ea9374104bf
0-0REL <rodrigo12337@outlook.com>
2025-07-12 18:34:42 -0600

    mahony

--- navio2/src/imu_mpu.cpp
+++ navio2/src/imu_mpu.cpp
@@ -8,7 +8,10 @@
 #include <cmath>
 #include <Common/MPU9250.h>
 #include <Common/Util.h>
-#include <MadgwickAHRS/MadgwickAHRS.h>
+//#include <MadgwickAHRS/MadgwickAHRS.h>
+extern "C"{
+    #include <MahonyAHRS/MahonyAHRS.h>
+}
 
 #define sampleFreq 510.0f
 
@@ -26,7 +29,7 @@ int main(int argc, char **argv)
     ros::Publisher mpu_pub = nh_mpu.advertise<geometry_msgs::Vector3>("ahrs_mpu",1000);
     ros::Rate r_mpu((int)sampleFreq); //100 Hz
     geometry_msgs::Vector3 msg;
-//    beta = 0.5f;
+//  beta = 0.5f;
     if (check_apm()) {
         return 1;
     }
@@ -44,7 +47,7 @@ int main(int argc, char **argv)
     float mx, my, mz;
     float roll, pitch, yaw;
     float gyroCal[3] = {0.0, 0.0, 0.0};
-//caslibracion
+//calibracion
     printf("calibrando...\n");
     for(int i = 0; i<100; i++)
     {
@@ -54,32 +57,34 @@ int main(int argc, char **argv)
 	gyroCal[1] += gy;
 	gyroCal[2] += gz;
 	usleep(10000);
-     }
-     gyroCal[0] /= 100;
-     gyroCal[1] /= 100;
-     gyroCal[2] /= 100;
-     printf("offset gyro %f %f %f\n", gyroCal[0], gyroCal[1], gyroCal[2]);
+    }
+    gyroCal[0] /= 100;
+    gyroCal[1] /= 100;
+    gyroCal[2] /= 100;
+    printf("offset gyro %f %f %f\n", gyroCal[0], gyroCal[1], gyroCal[2]);
 //-------------------------------------------------------------------------
-    while(ros::ok()) {
+    while(ros::ok()) 
+    {
 //sensores
         mpu.update();
         mpu.read_accelerometer(&ax, &ay, &az);
         mpu.read_gyroscope(&gx, &gy, &gz);
-//        mpu.read_magnetometer(&mx, &my, &mz);
+//      mpu.read_magnetometer(&mx, &my, &mz);
 //ahrs
         gx -= gyroCal[0];
         gy -= gyroCal[1];
         gz -= gyroCal[2];
-//        MadgwickAHRSupdate(gx,gy,gz,ax,ay,az,mx,my,mz);
-	MadgwickAHRSupdateIMU(gx,gy,gz,ax,ay,az);
-        getEuler(&roll, &pitch, &yaw);
-	msg.x = roll;
-	msg.y = pitch;
-	msg.z = yaw;
-	// se envia mensaje
-	ROS_INFO("Orientacion: x = %.2f, y = %.2f, z = %.2f", msg.x, msg.y, msg.z);
-	mpu_pub.publish(msg);
-	ros::spinOnce();
+//      MadgwickAHRSupdate(gx,gy,gz,ax,ay,az,mx,my,mz);
+	    //MadgwickAHRSupdateIMU(gx,gy,gz,ax,ay,az);
+        MahonyAHRSupdateIMU(gx,gy,gz,ax,ay,az);
+            getEuler(&roll, &pitch, &yaw);
+        msg.x = roll;
+        msg.y = pitch;
+        msg.z = yaw;
+        // se envia mensaje
+        ROS_INFO("Orientacion: x = %.2f, y = %.2f, z = %.2f", msg.x, msg.y, msg.z);
+        mpu_pub.publish(msg);
+        ros::spinOnce();
         r_mpu.sleep();
     }
 }
