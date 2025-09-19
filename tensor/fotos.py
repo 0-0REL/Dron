c9eb2b90966a19bfe26086ae5d6fc151e170159c
0-0REL <rodrigo12337@outlook.com>
2025-09-19 17:36:09 -0600

    dron

--- tensor/fotos.py
+++ tensor/fotos.py
@@ -1,87 +1,99 @@
+import mediapipe as mp
 import cv2
-import os
 import time
+import numpy as np
+import os
 
-# Configuración de la cámara
-source = cv2.VideoCapture(0)
-win_name = "Camera Preview"
-cv2.namedWindow(win_name, cv2.WINDOW_NORMAL)
+START_TIME = time.time()
+DETECTION_RESULT = None
 
-# Cargar modelo de detección facial
-net = cv2.dnn.readNetFromCaffe("complementos/modelos/deploy.prototxt", 
-                             "complementos/modelos/res10_300x300_ssd_iter_140000_fp16.caffemodel")
-
-# Parámetros del modelo
-in_width = 300
-in_height = 300
-mean = [104, 117, 123]
-conf_threshold = 0.7
+cam = cv2.VideoCapture(0)
 
 # Directorio para guardar rostros
-output_dir = "tensor/dataset/ignacio"
+output_dir = "tensor/dataset/rodri"
 if not os.path.exists(output_dir):
     os.makedirs(output_dir)
 
-frame_count = 51
+frame_count = 1
 last_face = None  # Para guardar el último rostro detectado
 
-while True:
-    has_frame, frame = source.read()
-    if not has_frame:
-        break
-    
-    frame = cv2.flip(frame, 1)
-    frame_height = frame.shape[0]
-    frame_width = frame.shape[1]
+BaseOptions = mp.tasks.BaseOptions
+FaceDetector = mp.tasks.vision.FaceDetector
+FaceDetectorOptions = mp.tasks.vision.FaceDetectorOptions
+FaceDetectorResult = mp.tasks.vision.FaceDetectorResult
+VisionRunningMode = mp.tasks.vision.RunningMode
 
-    # Preprocesamiento para la red neuronal
-    blob = cv2.dnn.blobFromImage(frame, 1.0, (in_width, in_height), mean, swapRB=False, crop=False)
-    net.setInput(blob)
-    detections = net.forward()
+# Create a face detector instance with the live stream mode:
+def print_result(result, output_image: mp.Image, timestamp_ms: int):
+    global DETECTION_RESULT
+    DETECTION_RESULT = result
+    #print('face detector result: {}'.format(result))
 
-    #for i in range(detections.shape[2]):
-    confidence = detections[0, 0, 0, 2]
-    if confidence > conf_threshold:
-        # Coordenadas del rostro detectado
-        x_top_left = int(detections[0, 0, 0, 3] * frame_width)
-        y_top_left = int(detections[0, 0, 0, 4] * frame_height)
-        x_bottom_right = int(detections[0, 0, 0, 5] * frame_width)
-        y_bottom_right = int(detections[0, 0, 0, 6] * frame_height)
+def visualize(image, detection_result) -> np.ndarray:
+    """Draws bounding boxes on the input image and return it.
+    Args:
+        image: The input RGB image.
+        detection_result: The list of all "Detection" entities to be visualized.
+    Returns:
+        Image with bounding boxes.
+    """
+    start_point = (0, 0)
+    end_point = (0, 0)
+    if detection_result is None:
+        return image, (0,0), (0,0)
         
-        # Asegurar que las coordenadas estén dentro del frame
-        x_top_left = max(0, x_top_left)
-        y_top_left = max(0, y_top_left)
-        x_bottom_right = min(frame_width, x_bottom_right)
-        y_bottom_right = min(frame_height, y_bottom_right)    
+    for detection in detection_result.detections:
+        # Draw bounding_box
+        bbox = detection.bounding_box
+        start_point = bbox.origin_x, bbox.origin_y
+        end_point = bbox.origin_x + bbox.width, bbox.origin_y + bbox.height
+        # Use the orange color for high visibility.
+        #print('Area: ', bbox.width * bbox.height)
+    
+    return image, start_point, end_point
 
-        # Recortar y redimensionar el rostro
-        face = frame[y_top_left:y_bottom_right, x_top_left:x_bottom_right]
-        if face.size > 0:  # Verificar que el recorte no esté vacío
-            face_resized = cv2.resize(face, (250, 250))
-            last_face = face_resized  # Guarda el último rostro detectado
+options = FaceDetectorOptions(
+    base_options=BaseOptions(model_asset_path='complementos/modelos/blaze_face_short_range.tflite'),
+    running_mode=VisionRunningMode.LIVE_STREAM,
+    result_callback=print_result)
 
-    key = cv2.waitKey(1) & 0xFF
-    if key == 27:  # ESC para salir
-        break
-    elif key == ord('s'):
-        if last_face is not None:
-            filename = f"{output_dir}/rostro_{frame_count}.jpg"
-            cv2.imwrite(filename, last_face)
-            print(f"Rostro guardado: {filename}")
-            frame_count += 1
-    # Dibujar rectángulo alrededor del rostro
-    cv2.rectangle(frame, (x_top_left, y_top_left), (x_bottom_right, y_bottom_right), (0, 255, 0), 2)
+st = (0,0)
+ed = (0,0)
+
+with FaceDetector.create_from_options(options) as detector:
+    while True:
+        ok, frm = cam.read()
+        if not ok:
+            print('Error')
+            break
             
-    # Mostrar información de tiempo
-    t, _ = net.getPerfProfile()
-    label = "Inference time: %.2f ms" % (t * 1000.0 / cv2.getTickFrequency())
-    cv2.putText(frame, label, (10, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0))
-    
-    # Contador de rostros guardados
-    cv2.putText(frame, f"Rostros guardados: {frame_count}", (10, 40), 
-               cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 255))
-    
-    cv2.imshow(win_name, frame) 
+        frm_rgb = cv2.cvtColor(frm, cv2.COLOR_BGR2RGB)
+        mp_image = mp.Image(image_format=mp.ImageFormat.SRGB, data=frm_rgb)
+        
+        frame_timestamp_ms = int((time.time() - START_TIME) * 1000)
+        # un ejemplo de mediapipe usa como time stamp -> time.time_ns() // 1_000_000
+        detector.detect_async(mp_image, frame_timestamp_ms)
+        
+        if DETECTION_RESULT:
+            frm, st, ed = visualize(frm, DETECTION_RESULT)
+            face = frm[st[1]:ed[1], st[0]:ed[0]]
+            if face.size > 0:  # Verificar que el recorte no esté vacío
+                #face_resized = cv2.resize(face, (250, 250))
+                last_face = face  # Guarda el último rostro detectado
 
-source.release()
+
+        key = cv2.waitKey(1) & 0xFF
+        if key == 27:  # ESC para salir
+            break
+        elif key == ord('s'):
+            if last_face is not None:
+                filename = f"{output_dir}/rostro_{frame_count}.jpg"
+                cv2.imwrite(filename, last_face)
+                print(f"Rostro guardado: {filename}")
+                frame_count += 1
+
+        cv2.rectangle(frm, st, ed, (0, 165, 255), 3)
+        cv2.imshow('face_detection', cv2.flip(frm,1))
+
+cam.release()
 cv2.destroyAllWindows()
--- tensor/prueba.py
+++ tensor/prueba.py
@@ -1,4 +1,6 @@
 import cv2
+import mediapipe as mp
+import time
 import numpy as np
 from ai_edge_litert.interpreter import Interpreter
 
@@ -7,42 +9,72 @@ interpreter.allocate_tensors()
 input_details = interpreter.get_input_details()
 output_details = interpreter.get_output_details()
 
-source = cv2.VideoCapture(0)
-win_name = "Camera Preview"
-cv2.namedWindow(win_name, cv2.WINDOW_NORMAL)
+START_TIME = time.time()
+DETECTION_RESULT = None
 
-net = cv2.dnn.readNetFromCaffe(
-    "complementos/modelos/deploy.prototxt",
-    "complementos/modelos/res10_300x300_ssd_iter_140000_fp16.caffemodel"
-)
-in_width = 300
-in_height = 300
-mean = [104, 117, 123]
-conf_threshold = 0.7
+cam = cv2.VideoCapture(0)
+BaseOptions = mp.tasks.BaseOptions
+FaceDetector = mp.tasks.vision.FaceDetector
+FaceDetectorOptions = mp.tasks.vision.FaceDetectorOptions
+FaceDetectorResult = mp.tasks.vision.FaceDetectorResult
+VisionRunningMode = mp.tasks.vision.RunningMode
 
-while cv2.waitKey(1) != 27:
-    has_frame, frame = source.read()
-    if not has_frame:
-        break
-    frame = cv2.flip(frame, 1)
-    frame_height = frame.shape[0]
-    frame_width = frame.shape[1]
+# Create a face detector instance with the live stream mode:
+def print_result(result, output_image: mp.Image, timestamp_ms: int):
+    global DETECTION_RESULT
+    DETECTION_RESULT = result
+    #print('face detector result: {}'.format(result))
 
-    blob = cv2.dnn.blobFromImage(frame, 1.0, (in_width, in_height), mean, swapRB=False, crop=False)
-    net.setInput(blob)
-    detections = net.forward()
+def visualize(image, detection_result) -> np.ndarray:
+    """Draws bounding boxes on the input image and return it.
+    Args:
+        image: The input RGB image.
+        detection_result: The list of all "Detection" entities to be visualized.
+    Returns:
+        Image with bounding boxes.
+    """
+    start_point = (0, 0)
+    end_point = (0, 0)
+    if detection_result is None:
+        return image, (0,0), (0,0)
+        
+    for detection in detection_result.detections:
+        # Draw bounding_box
+        bbox = detection.bounding_box
+        start_point = bbox.origin_x, bbox.origin_y
+        end_point = bbox.origin_x + bbox.width, bbox.origin_y + bbox.height
+        # Use the orange color for high visibility.
+        #print('Area: ', bbox.width * bbox.height)
+    
+    return image, start_point, end_point
 
-    for i in range(detections.shape[2]):
-        confidence = detections[0, 0, i, 2]
-        if confidence > conf_threshold:
-            x_top_left = int(detections[0, 0, i, 3] * frame_width)
-            y_top_left = int(detections[0, 0, i, 4] * frame_height)
-            x_bottom_right = int(detections[0, 0, i, 5] * frame_width)
-            y_bottom_right = int(detections[0, 0, i, 6] * frame_height)
+options = FaceDetectorOptions(
+    base_options=BaseOptions(model_asset_path='complementos/modelos/blaze_face_short_range.tflite'),
+    running_mode=VisionRunningMode.LIVE_STREAM,
+    result_callback=print_result)
 
-            # Recorta y prepara el rostro para el modelo TFLite
-            face = frame[y_top_left:y_bottom_right, x_top_left:x_bottom_right]
-            if face.size > 0:
+st = (0,0)
+ed = (0,0)
+
+with FaceDetector.create_from_options(options) as detector:
+    while cv2.waitKey(1) != 27:
+        ok, frm = cam.read()
+        if not ok:
+            print('Error')
+            break
+        frm = cv2.flip(frm, 1)
+            
+        frm_rgb = cv2.cvtColor(frm, cv2.COLOR_BGR2RGB)
+        mp_image = mp.Image(image_format=mp.ImageFormat.SRGB, data=frm_rgb)
+        
+        frame_timestamp_ms = int((time.time() - START_TIME) * 1000)
+        # un ejemplo de mediapipe usa como time stamp -> time.time_ns() // 1_000_000
+        detector.detect_async(mp_image, frame_timestamp_ms)
+        
+        if DETECTION_RESULT:
+            frm, st, ed = visualize(frm, DETECTION_RESULT)
+            face = frm[st[1]:ed[1], st[0]:ed[0]]
+            if face.size > 0:  # Verificar que el recorte no esté vacío
                 face_resized = cv2.resize(face, (160, 160))
                 face_resized = cv2.cvtColor(face_resized, cv2.COLOR_BGR2RGB)
                 face_array = face_resized.astype(np.float32) / 255.0
@@ -54,28 +86,18 @@ while cv2.waitKey(1) != 27:
                 clase = np.argmax(output[0])
                 prob = output[0][clase]
 
-                # Nueva lógica de umbral
-                if clase == 1 and prob >= 0.7:
-                    nombre = "Ignacio"
-                    color = (255, 255, 0)
-                elif clase == 2 and prob >= 0.7:
-                    nombre = "Rodrigo"
+                caras = {0: "ignacio", 1: "Isis", 2: "Rodri", 3: "Rodrigo"}
+
+                if prob >= 0.55:
+                    nombre = caras[clase]
                     color = (0, 255, 0)
                 else:
                     nombre = "Desconocido"
                     color = (0, 0, 255)
-                    clase = 0  # fuerza clase 0 si no supera el umbral
-
                 label = f"{nombre} ({prob:.2f})"
-                cv2.rectangle(frame, (x_top_left, y_top_left), (x_bottom_right, y_bottom_right), color, 2)
-                cv2.putText(frame, label, (x_top_left, y_top_left - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.7, color, 2)
-            else:
-                cv2.rectangle(frame, (x_top_left, y_top_left), (x_bottom_right, y_bottom_right), (0, 255, 0), 2)
+                cv2.putText(frm, label, (st[0], st[1] - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.7, color, 2)
+        cv2.rectangle(frm, st, ed, (0, 165, 255), 3)
+        cv2.imshow('face_detection', frm)
 
-    t, _ = net.getPerfProfile()
-    label = "Inference time: %.2f ms" % (t * 1000.0 / cv2.getTickFrequency())
-    cv2.putText(frame, label, (0, 15), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0))
-    cv2.imshow(win_name, frame)
-
-source.release()
-cv2.destroyWindow(win_name)
+cam.release()
+cv2.destroyAllWindows()
