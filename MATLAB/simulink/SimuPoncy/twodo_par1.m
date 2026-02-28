822bab757d87c123142b4f8f8523866a64488647
0-0REL <rodrigo12337@outlook.com>
2026-02-28 08:19:54 -0600

    poncy

--- MATLAB/simulink/SimuPoncy/twodo_par1.m
+++ MATLAB/simulink/SimuPoncy/twodo_par1.m
@@ -1,28 +1,29 @@
 %PID parameters
 %pidp2or
-clear all;
+clc
+clear;
 close all;
 
-St=0.01;
+St=0.01; % Tiempo de muestreo 0.01
 Tm=St;
 SP=1;
 LD=1*SP;
-Delay=0.4;
+Delay=0.00001; %0.4
 Kp=12.57;
 Ti=0.22*4;
 Td=0.04*4;
 b=1-0.64;
 c=1-0.66;
 GE=1;
-GCE=0.385;  %2*Td;
-GU=4.028;  %Kp/2;
-GCU=10.46;  %Kp/Ti;
+GCE=2*Td;  %2*Td;0.385
+GU=Kp/2;  %Kp/2;4.028
+GCU=Kp/Ti;  %Kp/Ti;10.46
 stfc22=readfis('stfc22.fis');
 stfc23=readfis('stfc23.fis');
-p1=1;  %3.2;
-q1=1;  %3.2;
-p2=1;  %0;
-q2=1;  %0;
+p1=3.2;  %3.2;1
+q1=3.2;  %3.2;1
+p2=0;  %0;1
+q2=0;  %0;1
 p4=p1;
 q4=q1;
 
@@ -45,3 +46,12 @@ GCE2=Kd/GU2;
 
 
 
+gk = 9.81; % gravedad
+md = 1.6; % masa del dron
+Jn = [0.021915027 0.022124548 0.042349865]; % inecia en ejes principales
+kf = 0.7*9.81; % coeficiente de empuje
+km = 0.2; % coeficiente de arrastre
+l = 0.268554; % largo de los brazos
+om_0 = sqrt(md*gk/(4*kf)); % velocidad de motores con dron flotando
+om_1 = sqrt(md*gk/(4*km)); % velocidad de motores dron rumbo fijo
+mk = log(1-0.9933)/-0.1; % retraso motores
